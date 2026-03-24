2192f13fcb4521bbce420ecdd8d6af2a5f78ac62
nniecikowski <niecikowski.n01@mymail.sxu.edu>
2026-03-24 13:38:52 -0500

    Changes

--- backend/src/routes/healthRoutes.js
+++ backend/src/routes/healthRoutes.js
@@ -5,43 +5,36 @@ const router = express.Router();
 
 router.get("/pastdata", async (req, res) => {
     try {
-        const profileId = 1;
-        
+        const profileId = req.query.profile_id;
+
         //weight
-        console.log("Running weight...");
-        const [weightRows] = await db_pool.query(
-            `SELECT weight AS weight, recorded_at AS recordAt
-            FROM personal_information 
-            WHERE profile_id =?
-            ORDER BY recorded_at ASC`, [profileId]
+        const [weights] = await db_pool.query(
+            `SELECT weight, recorded_at AS recordAT
+             FROM personal_information
+             WHERE profile_id = ?` , 
+             [profileId]
         );
-        console.log("Weights:", weightRows);
 
         //nutrition
-        console.log("Running nutrition...");
-        const [nutritionRows] = await db_pool.query(
-            `SELECT diet_name, calories, protein, carbohydrates AS carbs, recorded_at AS recordAt
-            FROM diets
-            WHERE profile_id = ?
-            ORDER BY recorded_at ASC`, [profileId]
+        const [nutrition] = await db_pool.query(
+            `SELECT recordAt, calories, protein, carbs 
+             FROM diets
+             WHERE profile_id = ?`,
+             [profileId] 
         );
-        console.log("Nutrition: ", nutritionRows);
 
         //exercise
-        console.log("Running exercise...");
-        const [exerciseRows] = await db_pool.query(
-            `SELECT exercise_type, sets, reps, weight recorded_at AS recordAt
-            FROM exercise 
-            WHERE profile_id = ?
-            ORDER BY recorded_at`, [profileId]
+        const [exercise] = await db_pool.query(
+            `SELECT recorded_at AS recordAT, exercise_type, sets, reps, weight
+             FROM exercise
+             WHERE profile_id = ?`, 
+             [profileId]
         );
-        console.log("Exercise: ", exerciseRows);
-
 
         res.json({
-            weights: weightRows,
-            nutrition: nutritionRows,
-            exercise: exerciseRows
+            weights,
+            nutrition,
+            exercise
         });
     }
     catch (err) {
