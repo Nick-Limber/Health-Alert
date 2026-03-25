9ed88937053d9300d23fb9fa9a9d17879eb3ad7d
pchavez22 <137192941+pchavez22@users.noreply.github.com>
2026-03-25 18:39:08 -0500

    Revert "Porfirio dev"

--- HealthAlert2/app/src/main/java/com/example/healthalert2/data/network/MembershipResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.healthalert2.data.network
-data class MembershipResponse(
-    val status: String,
-    val plan: String
-)
--- backend/src/routes/membershipRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { toggleMembership, getMembership } from "../controllers/membershipController.js";
-
-const router = express.Router();
-
-router.post("/toggle", toggleMembership);
-router.get("/:id", getMembership)
-export default router;
